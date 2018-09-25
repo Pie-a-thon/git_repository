63eec99d7a859cb74f2ffde8c458ece7c915ba04
Pie-a-thon <43213637+Pie-a-thon@users.noreply.github.com>
2018-09-25 11:44:27 +0100

    optimized and added tests to hough, nn works with keras data

--- NN.py
+++ NN.py
@@ -1,3 +1,4 @@
+from trackml.dataset import load_event
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
@@ -5,7 +6,6 @@ import random
 import math as m
 import tensorflow as tf
 from tensorflow import keras
-
 
 def load_mu(path_to_folder, number):
     list = np.linspace(1,5000, num=5000, dtype=int)
@@ -23,43 +23,87 @@ def load_mu(path_to_folder, number):
 
     return hits
 
+def muons():
 
-data = load_mu(".\cernbox\inputs_ATLAS_step3_26082018/mu1GeV", 100)
-features = (data[['x','y','z']]/1000.0)
-particle_ids = data.labels.unique()
-particle_ids = particle_ids[np.where(particle_ids!=0)[0]]
+    data = load_mu("..\cernbox\inputs_ATLAS_step3_26082018/mu1GeV", 100)
+    features = (data[['x','y','z']]/1000.0)
+    particle_ids = data.labels.unique()
+    particle_ids = particle_ids[np.where(particle_ids!=0)[0]]
 
-Train = []
-pair = []
-for particle_id in particle_ids:
-    hit_ids = data.index.values[data.labels == particle_id]
-    for i in hit_ids:
-        for j in hit_ids:
-            if i != j:
-                pair.append([i,j])
+    Train = []
+    pair = []
+    for particle_id in particle_ids:
+        hit_ids = data.index.values[data.labels == particle_id]
+        for i in hit_ids:
+            for j in hit_ids:
+                if i != j:
+                    pair.append([i,j])
 
-pair = np.array(pair)
-Train1 = np.hstack((features.values[pair[:,0]], features.values[pair[:,1]], np.ones((len(pair),1))))
+    pair = np.array(pair)
+    Train1 = np.hstack((features.values[pair[:,0]], features.values[pair[:,1]], np.ones((len(pair),1))))
 
-if len(Train) == 0:
-        Train = Train1
-else:
-    Train = np.vstack((Train,Train1))
+    if len(Train) == 0:
+            Train = Train1
+    else:
+        Train = np.vstack((Train,Train1))
 
-n = len(data)
-size = len(Train1)*3
-p_id = data.labels.values
-i =np.random.randint(n, size=size)
-j =np.random.randint(n, size=size)
-pair = np.hstack((i.reshape(size,1),j.reshape(size,1)))
-pair = pair[((p_id[i]==0) | (p_id[i]!=p_id[j]))]
-
+    n = len(data)
+    size = len(Train1)*3
+    p_id = data.labels.values
+    i =np.random.randint(n, size=size)
+    j =np.random.randint(n, size=size)
+    pair = np.hstack((i.reshape(size,1),j.reshape(size,1)))
+    pair = pair[((p_id[i]==0) | (p_id[i]!=p_id[j]))]
+'''
 Train0 = np.hstack((features.values[pair[:,0]], features.values[pair[:,1]], np.zeros((len(pair),1))))
 
 Train = np.vstack((Train,Train0))
 
 np.random.shuffle(Train)
-print(Train)
+print(Train)'''
+#################################################################################################################
+#################################################################################################################
+
+hits, cells, particles, truth = load_event("../train_sample/train_100_events/event000001000")
+hit_cells = cells.groupby(['hit_id']).value.count().values
+hit_value = cells.groupby(['hit_id']).value.sum().values
+features = np.hstack((hits[['x','y','z']]/1000, hit_cells.reshape(len(hit_cells),1)/10,hit_value.reshape(len(hit_cells),1)))
+particle_ids = truth.particle_id.unique()
+particle_ids = particle_ids[np.where(particle_ids!=0)[0]]
+Train=[]
+pair = []
+for particle_id in particle_ids:
+    hit_ids = truth[truth.particle_id == particle_id].hit_id.values-1
+    for i in hit_ids:
+        for j in hit_ids:
+            if i != j:
+                pair.append([i,j])
+pair = np.array(pair)
+Train1 = np.hstack((features[pair[:,0]], features[pair[:,1]], np.ones((len(pair),1))))
+
+if len(Train) == 0:
+    Train = Train1
+else:
+    Train = np.vstack((Train,Train1))
+
+n = len(hits)
+size = len(Train1)*3
+p_id = truth.particle_id.values
+i =np.random.randint(n, size=size)
+j =np.random.randint(n, size=size)
+pair = np.hstack((i.reshape(size,1),j.reshape(size,1)))
+pair = pair[((p_id[i]==0) | (p_id[i]!=p_id[j]))]
+
+Train0 = np.hstack((features[pair[:,0]], features[pair[:,1]], np.zeros((len(pair),1))))
+
+Train = np.vstack((Train,Train0))
+del Train0, Train1
+
+np.random.shuffle(Train)
+print(Train.shape)
+
+
+
 
 '''input_list = []
 truth_list = []
@@ -79,7 +123,7 @@ input_array = np.array(input_list)
 truth_array = np.array(truth_list)
 '''
 
-'''model = keras.Sequential([
+model = keras.Sequential([
     keras.layers.Dense(6),
     keras.layers.Dense(800, activation="selu"),
     keras.layers.Dense(400, activation="selu"),
@@ -87,9 +131,9 @@ truth_array = np.array(truth_list)
     keras.layers.Dense(400, activation="selu"),
     keras.layers.Dense(200, activation="selu"),
     keras.layers.Dense(1, activation = "sigmoid") #activation="sigmoid"
-])'''
+])
 
-'''lr=-5
+lr=-5
 model.compile(loss=['binary_crossentropy'], optimizer=keras.optimizers.Adam(lr=10**(lr)), metrics=['accuracy'])
 History = model.fit(x=Train[:,:-1], y=Train[:,-1], batch_size=8000, epochs=1, verbose=2, validation_split=0.05, shuffle=True)
 
@@ -100,16 +144,16 @@ History = model.fit(x=Train[:,:-1], y=Train[:,-1], batch_size=8000, epochs=20, v
 lr=-5
 model.compile(loss=['binary_crossentropy'], optimizer=keras.optimizers.Adam(lr=10**(lr)), metrics=['accuracy'])
 History = model.fit(x=Train[:,:-1], y=Train[:,-1], batch_size=8000, epochs=3, verbose=2, validation_split=0.05, shuffle=True)
-'''
-
-#model.save('my_model.h5')
 
 
-new_model = keras.models.load_model('my_model.h5')
+model.save('my_model_with_cells.h5')
+
+
+'''new_model = keras.models.load_model('../my_model.h5')
 new_model.summary()
 test = new_model.evaluate(x=Train[:,:-1], y=Train[:,-1],verbose = 1)
 print(new_model.metrics_names)
-print(test)
+print(test)'''
 
 '''predictions = new_model.predict(input_array)
 print(predictions)'''
--- hough_transform.py
+++ hough_transform.py
@@ -7,21 +7,31 @@ import random
 import math as m
 import scipy as sp
 
-def load_mu(path_to_folder, number):
+def load_mu(path_to_folder, number, full_out=False):
     list = np.linspace(1,5000, num=5000, dtype=int)
 
     samples = random.sample(set(list), number)
 
     hits = pd.DataFrame(columns = ["x", "y", "z", "labels", "other"])
+    truth = pd.DataFrame(columns=["barcode", "pt", "eta", "phi", "e", "labels"])
 
     for i in samples:
         df=pd.read_csv(path_to_folder+"/clusters_"+str(i)+".csv", sep=',', names=["x", "y", "z", "labels", "other", "track"])
+        df_truth=pd.read_csv(path_to_folder+"/truth_"+str(i)+".csv", sep=',', names=["barcode", "pt", "eta", "phi", "e", "labels"])
+        row = (df_truth.iloc[0].values)
+        df_truth = pd.DataFrame({"barcode":row[0], "pt":row[1], "eta":row[2], "phi":row[3], "e":row[4], "labels":row[5]}, index=[1])
         df = df.fillna(0.0)
         sig = df.labels != 0.0
         df.labels[sig] = i
         hits = hits.append(df, ignore_index=True)
+        truth = truth.append(df_truth)
 
-    return hits
+    truth.reset_index(drop=True, inplace=True)
+    if full_out:
+        return hits, truth
+
+    else:
+        return hits
 
 def bin_plot(hits, num_bins=200, binary=False, mapped=False):
     if mapped:
@@ -33,46 +43,6 @@ def bin_plot(hits, num_bins=200, binary=False, mapped=False):
         mask = hist != 0
         hist[mask] = 1
     return hist
-
-def score(tracks, hits):
-    score_list = []
-    for hit_indices in tracks.candidate_hits:
-        score = 0.0
-        if len(hit_indices) == 0:
-            score = 0.0
-        else:
-            true_hits = hits.loc[hits["labels"] != 0.0]
-            particle_labels = np.unique(true_hits.labels.data)
-            for label in particle_labels:
-                particle_hits = true_hits.loc[true_hits["labels"] == label]
-                match = len(set(hit_indices).intersection(particle_hits.index.values))
-                if match/len(particle_hits.index.values) > 0.5 and match/len(hit_indices) > 0.5:
-                    score = match/len(particle_hits)
-    score_list.append(score)
-
-    avg_score = sum(score_list)/len(score_list)
-    return score_list, avg_score
-
-def line_score(hits, lines):
-    score_list = []
-    for i in range(0, len(lines)):
-        score = 0.0
-        track_indices = hits.loc[(hits["track"] == i)].index.values
-        if len(track_indices) == 0:
-            score = 0.0
-        else:
-            true_hits = hits.loc[hits["labels"] >= 0.0]
-            particle_labels = np.unique(true_hits.labels.data)
-            for label in particle_labels:
-                particle_hits = true_hits.loc[true_hits["labels"] == label]
-                match = len(set(track_indices).intersection(particle_hits.index.values))
-                if match/len(particle_hits.index.values) > 0.5 and match/len(track_indices) > 0.5:
-                    score = match/len(particle_hits)
-
-        score_list.append(score)
-
-    avg_score = sum(score_list)/len(score_list)
-    return score_list, avg_score
 
 def conformal_map(hits, x0=0, y0=0):
     x = hits.x.values
@@ -87,6 +57,7 @@ def conformal_map(hits, x0=0, y0=0):
         v = (y[i]-y0)/r_sq
         phi = np.arctan2(v, u)
         rho = m.sqrt(u**2 + v**2)
+        d = u*np.cos(phi) + v*np.sin(phi)
         u_list.append(u)
         v_list.append(v)
         phi_list.append(phi)
@@ -134,6 +105,7 @@ def get_line_tracks(hits, lines, max_dist=None):
     for i in range(0, len(hits.index.values)):
         u = hits.u.values[i]
         v = hits.v.values[i]
+        phi = hits.phi.values[i]
 
         dist_square_list = []
         for j in range(0, len(lines.index.values)):
@@ -154,23 +126,113 @@ def get_line_tracks(hits, lines, max_dist=None):
         #val, idx = min((val, idx) for (idx, val) in enumerate(dist_square_list))
         hits.track[i] = track
 
+def line_score(hits, lines):
+    score_list = []
+    for i in range(0, len(lines)):
+        score = 0.0
+        track_indices = hits.loc[(hits["track"] == i)].index.values
+        if len(track_indices) == 0:
+            score = 0.0
+        else:
+            true_hits = hits.loc[hits["labels"] >= 0.0]
+            particle_labels = np.unique(true_hits.labels.data)
+            for label in particle_labels:
+                particle_hits = true_hits.loc[true_hits["labels"] == label]
+                match = len(set(track_indices).intersection(particle_hits.index.values))
+                if match/len(particle_hits.index.values) > 0.5 and match/len(track_indices) > 0.5:
+                    score = match/len(particle_hits)
 
-data = load_mu("..\cernbox\inputs_ATLAS_step3_26082018/mu1GeV", 100)
+        score_list.append(score)
+
+    avg_score = sum(score_list)/len(score_list)
+    return score_list, avg_score
+
+def line_test(hist, num_bins, num_angles, num_hits):
+    lines = get_lines(hist, num_bins=num_bins, num_angles=num_angles, thresh=0.0, max_dist=0.001)
+    res = (num_hits-len(lines.index.values))**2
+    return res
+
+def lines_optim(min=100, max=400, num=10):
+    # columns=num_bins, rows=num_angles
+    num_hits = np.linspace(min, max, num =num)
+    for i in num_hits:
+        data = load_mu("..\cernbox\inputs_ATLAS_step3_26082018/mu1GeV", int(i))
+        conformal_map(data)
+        columns = np.linspace(500, 2000, num=20)
+        rows = np.linspace(500, 2000, num = 20)
+
+        heatmap = pd.DataFrame(0.0, columns=columns, index=rows)
+        for j in columns:
+            for k in rows:
+                hist = bin_plot(data, num_bins=j, binary=True, mapped=True)
+                cell = line_test(hist, j, k, int(i))
+                heatmap[j][k] = cell
+                print(heatmap)
+
+                heatmap.to_csv("bins_angles_"+str(int(i))+".csv")
+
+def avg_number():
+    heatmap = pd.read_csv("bins_angles_100.csv", sep=',', index_col =0)
+
+
+    for i in np.linspace(100, 400, num=10):
+        df = pd.read_csv("bins_angles_"+str(int(i))+".csv", sep=',', index_col =0)
+        heatmap = heatmap + df
+
+    print(heatmap)
+
+    plt.imshow(heatmap, interpolation="gaussian")
+    plt.show()
+
+    print(heatmap.min(1), heatmap.min(0))
+
+def eta_test():
+    eta_list = []
+    scores = []
+    for i in range(0, 200):
+        data, truth = load_mu("..\cernbox\inputs_ATLAS_step3_26082018/mu1GeV", 5, full_out=True)
+        conformal_map(data)
+        hist = bin_plot(data, num_bins=500, binary=True, mapped=True)
+        lines = get_lines(hist, num_bins=500, num_angles=900)
+        tracks = get_line_tracks(data, lines)
+        score_list, avg_score = line_score(data, lines)
+        print(score_list)
+        print(avg_score)
+
+        avg_eta = sum(truth.eta.values)/len(truth.eta.values)
+        scores.append(avg_score)
+        eta_list.append(avg_eta)
+
+    print(sum(scores)/len(scores))
+    plt.scatter(eta_list, scores)
+    plt.show()
+
+eta_test()
+
 hits, cells, particles, truth  = load_event("../train_sample/train_100_events/event000001000")
 
-conformal_map(data)
-conformal_map(hits)
+'''conformal_map(hits)
 
 X = [8, 0.001, 0.0002]
 thresh=int(X[0])
 max_from_center=X[1]
 max_dist_from_line=X[2]
 
-hist = bin_plot(data, num_bins=1000, binary=True, mapped=True)
 
-lines = get_lines(hist, num_bins = 1000, num_angles=800, thresh=thresh, max_dist=max_from_center)
+lines = get_lines(hist, num_bins = 1053, num_angles=1431, thresh=thresh, max_dist=max_from_center)'''
 
-def tracks(thresh):
+'''data = load_mu("..\cernbox\inputs_ATLAS_step3_26082018/mu1GeV", 400)
+conformal_map(data)
+hist = bin_plot(data, num_bins=1600, binary=True, mapped=True)
+print(line_test(hist, 1600, 3600, 400))'''
+
+
+#lines_optim()
+
+
+#tracks = get_line_tracks(data, lines)
+
+'''def tracks(thresh):
     lines = get_lines(hist, num_bins = 1000, num_angles=800, thresh=int(thresh), max_dist=0.001)
     get_line_tracks(data, lines, max_dist=0.0001)
 
@@ -184,14 +246,27 @@ thresholds = np.linspace(0, 20, num=20)
 scores = np.vectorize(tracks)(thresholds)
 
 plt.plot(thresholds, scores)
-plt.show()
+plt.show()'''
 
 
 #optimize = sp.optimize.minimize(transform, x0=[8, 0.001, 0.0002], method="Nelder-Mead", bounds=[(0.0, None), (0.0, None), (0.0, None)])
 #print(optimize.x)
 
+data, truth = load_mu("..\cernbox\inputs_ATLAS_step3_26082018/mu1GeV", 5, full_out=True)
+print(data)
+print(truth)
+conformal_map(data)
+hist = bin_plot(data, num_bins=1600, binary=True, mapped=True)
+lines = get_lines(hist, num_bins=1600, num_angles=3600)
+tracks = get_line_tracks(data, lines)
 
-'''fig, ax = plt.subplots()
+print(lines)
+
+score_list, avg_score = line_score(data, lines)
+print(score_list)
+print(avg_score)
+
+fig, ax = plt.subplots()
 for m, c in zip(lines.gradient.values, lines.intercept.values):
         y0 = m*(-0.03) + c
         y1 = m*(0.03) + c
@@ -215,4 +290,4 @@ ax.set_xlim((0, hist.shape[1]))
 ax.set_ylim((0, hist.shape[0]))
 ax.set_title('Detected lines')
 
-plt.show()'''
+plt.show()
